1ca33c7572b8157b7faed267da768ec716fe80b5
gushikem01 <gl_higaisha@yahoo.co.jp>
2024-04-08 01:53:03 +0900

    fix: filter stock

--- lib/stocks/stocks.ex
+++ lib/stocks/stocks.ex
@@ -4,12 +4,13 @@ defmodule Stocks.Stock do
 
   @primary_key {:id, :binary_id, autogenerate: true}
   schema "stocks" do
-    field :symbol, :string, default: ""
-    field :name, :string, default: ""
-    field :price, :decimal, default: 0
     field :exchange, :string, default: ""
     field :exchange_short_name, :string, default: ""
     field :is_etf, :boolean, default: false
+    field :market_cap, :decimal, default: 0
+    field :name, :string, default: ""
+    field :price, :decimal, default: 0
+    field :symbol, :string, default: ""
 
   end
 end
--- lib/stocks_web/controllers/page_controller.ex
+++ lib/stocks_web/controllers/page_controller.ex
@@ -1,13 +1,22 @@
 defmodule StocksWeb.PageController do
   use StocksWeb, :controller
 
+  # alias Ecto.Query.Builder.OrderBy
+  alias Ecto.Query.Builder.OrderBy
   alias Stocks.Stock
   alias Stocks.Repo
 
+  @spec home(Plug.Conn.t(), any()) :: Plug.Conn.t()
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    stocks = Repo.all(Stock)
+    stocks = Stock
+      |> Repo.all(
+        order_by: [asc: :market_cap]
+      )
+      |> Enum.sort_by(& &1.market_cap, &>=/2)
+      |> Enum.take(10)
     render(conn, :home, layout: false, stocks: stocks)
   end
+
 end
